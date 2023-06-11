adfac897ae60e2a1bbc888cab727b5ebcc685a14
abdullox0900 <abdullox0900@gmail.com>
2023-06-11 23:15:34 +0500

    feat: update

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,58 @@
+import { useEffect, useState } from 'react'
 import Login from '../screen/login/login'
 import './App.css'
 import Navbar from './components/navbar/navbar.jsx'
 
+import axios from 'axios'
+
 function App() {
+
+  const [data, setData] = useState();
+  const [loading, setLoading] = useState(true);
+  const [error, setError] = useState(false)
+
+  async function dataFetch() {
+    try {
+      const { data } = await axios.get('https://jsonplaceholder.typicode.com/users');
+      setData(data)
+    } catch {
+      setError(true)
+    } finally {
+      setLoading(false)
+    }
+  }
+
+  console.log(data);
+
+  useEffect(() => {
+    dataFetch()
+  }, [])
+
+  if (loading) {
+    return <h1>Loading...</h1>
+  }
+
+  if (error) {
+    return <h1>Error</h1>
+  }
 
   return (
     <>
-      <Navbar />
+      {/* <Navbar /> */}
+
+    {
+      data?.map((item, index) => {
+        return(
+         <>
+          <p>Name: {item.name}</p>
+          <p>UserName: {item.username}</p>
+          <br />
+          <br />
+         </>
+        )
+      })
+    }
+
     </>
   )
 }
